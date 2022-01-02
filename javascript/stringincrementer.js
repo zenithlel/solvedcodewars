23c48ca73d424a86b45f948304da06c55d072a05
Mohamed <83914557+zenithlel@users.noreply.github.com>
2022-01-02 14:05:37 +0200

    Update stringincrementer.js

--- javascript/stringincrementer.js
+++ javascript/stringincrementer.js
@@ -34,3 +34,9 @@ function incrementString(str){
             default: return str+"1";
                 }
 }
+
+
+function incrementString (strng) {
+  return strng.replace(/[\d]*$/, i => String(+i + 1).padStart(i.length, 0));
+
+}

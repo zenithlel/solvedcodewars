73f3b4e6f4036132f697b9320409d8e3fd8842e7
Mohamed <83914557+zenithlel@users.noreply.github.com>
2022-01-05 01:52:11 +0200

    Update makethedeadfishswim.js

--- javascript/makethedeadfishswim.js
+++ javascript/makethedeadfishswim.js
@@ -83,3 +83,7 @@ const parse = data => data
 
       return { val, output }
     }, { val: 0, output: [] }).output
+
+
+const parse = data =>
+  [...data].reduce(([a, b], val) => val === `i` ? [++a, b] : val === `d` ? [--a, b] : val === `s` ? [a ** 2, b] : val === `o` ? [a, [...b, a]] : [a, b], [0, []])[1];
